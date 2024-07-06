9dc0dc6ca2ba130cd4e74adf2e6a7cf1f1d49eb2
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-07-06 17:37:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -371,7 +371,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 user = await bot.get_chat_member(channel_chat_id, cmd.message.chat.id)
                 if user.status == "kicked":
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/VJ_Bot_Disscussion).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/KF_Cinemas_Kannada).",
                         disable_web_page_preview=True
                     )
                     return
