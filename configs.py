447c44a7731ff1c800ac83444bbcc78cf7e65ecc
moviessworld <ujwalnayak74@gmail.com>
2024-07-06 20:10:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "3r4NNBapROWiiOZHPEyNK9PM8oj1")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1981280736"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://kfcinemas:ujwalu@kannadamovieofficial.1jr4puu.mongodb.net/?retryWrites=true&w=majority&appName=KannadaMovieofficial")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002211169297")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002094674022")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002163064206"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
